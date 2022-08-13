3dcd240bde9096dea39d14a9760a445e1b66c8e9
Lars Martin Risdal <larsrisdal@gmail.com>
2022-08-13 15:54:36 +0200

    add eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing'
+import { AppComponent } from './app.component'
 
-const appComponentFixture = TestBed.createComponent(AppComponent);
-const appComponent = appComponentFixture.debugElement.componentInstance;
+const appComponentFixture = TestBed.createComponent(AppComponent)
+const appComponent = appComponentFixture.debugElement.componentInstance
 
 test('should create', () => {
-  expect(appComponent).toBeTruthy();
-});
+  expect(appComponent).toBeTruthy()
+})
 
 test(`the title is 'ConnXio`, () => {
-  expect(appComponent.title).toEqual('ConnXio');
-});
+  expect(appComponent.title).toEqual('ConnXio')
+})
