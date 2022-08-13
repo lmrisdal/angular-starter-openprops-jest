28f39472519420ed4cabc489681301eabd34aa71
Lars Martin Risdal <larsrisdal@gmail.com>
2022-08-13 14:12:14 +0200

    added project with jest testing

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+const appComponentFixture = TestBed.createComponent(AppComponent);
+const appComponent = appComponentFixture.debugElement.componentInstance;
+
+test('should create', () => {
+  expect(appComponent).toBeTruthy();
+});
+
+test(`the title is 'ConnXio`, () => {
+  expect(appComponent.title).toEqual('ConnXio');
+});
